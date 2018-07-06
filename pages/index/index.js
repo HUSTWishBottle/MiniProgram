6767a00906523e6f9a585cecdea7883ae9b11dab
Huang <1876721012@qq.com>
2018-07-06 11:44:14 +0800

    tree

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,21 +12,17 @@ Page({
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../login/login'
     })
   },
-  login: function () {
-    console.log("进入登录界面")
-    wx.navigateTo({
-      url: '../login/login',
-    })
-  },
+  
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
+        
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -48,6 +44,7 @@ Page({
         }
       })
     }
+    
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -55,6 +52,12 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    }),
+    setTimeout(function () {
+      wx.navigateTo({
+        url: '../login/login',
+      })
     })
   }
+  
 })
--- pages/tree/tree.js
+++ pages/tree/tree.js
@@ -1,116 +1,77 @@
-// pages/tree/tree.js
+// pages/wishSea/wishSea.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    indicatorDots: false,
-    autoplay: false,
-    interval: 5000,
-    duration: 1000,
-    a: [{
+    nodes: [{
       name: 'div',
       attrs: {
         class: 'div_class',
-        style: 'line-height: 20px; color: red;font-size:20pt;'
+        style: 'line-height: 20px;color:red;font-size:20pt;'
       },
       children: [{
         type: 'text',
-        text: '我的心愿'
+        text: '标题'
       }]
     }],
-    b: [{
-      name: 'div',
-      attrs: {
-        class: 'div_class',
-        style: 'line-height: 20px; color: green;font-size:20pt;'
-      },
-      children: [{
-        type: 'text',
-        text: '我的评论'
-      }]
-    }],
-    c: [{
-      name: 'div',
-      attrs: {
-        class: 'div_class',
-        style: 'line-height: 20px; color: red;font-size:20pt;'
-      },
-      children: [{
-        type: 'text',
-        text: '我的收藏'
-      }]
-    }],
-    wishes:[
-      {
-        title:"wish1",
-        concent:"this is my wish"
-      },
-      {
-        title: "wish2",
-        concent: "this is his wish"
-      },
-      {
-        title: "wish3",
-        concent: "this is her wish"
-      }
-    ]
-  },
+  },  
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
